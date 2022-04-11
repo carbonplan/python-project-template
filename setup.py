c13a5635f7e93266c59de1bf53a2dd57807c000e
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-04-11 09:53:43 -0600

    Update pre-commit-hooks (#22)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,33 +2,32 @@
 
 """The setup script"""
 
+
+import pathlib
+
 from setuptools import find_packages, setup
 
-with open("requirements.txt") as f:
-    INSTALL_REQUIRES = f.read().strip().split("\n")
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = f.read().strip().split('\n')
 
-with open("README.md") as f:
-    LONG_DESCRIPTION = f.read()
+LONG_DESCRIPTION = pathlib.Path('README.md').read_text()
+PYTHON_REQUIRES = '>=3.7'
 
-PYTHON_REQUIRES = ">=3.7"
-
-description = "carbonplan python project template"
+description = 'carbonplan python project template'
 
 setup(
-    name="carbonplan-project",
+    name='carbonplan-project',
     description=description,
     long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    maintainer="Raphael Hagen",
-    maintainer_email="raphael@carbonplan.org",
-    url="https://github.com/carbonplan/python-project-template",
+    long_description_content_type='text/markdown',
+    maintainer='Raphael Hagen',
+    maintainer_email='raphael@carbonplan.org',
+    url='https://github.com/carbonplan/python-project-template',
     packages=find_packages(),
     include_package_data=True,
     python_requires=PYTHON_REQUIRES,
     install_requires=INSTALL_REQUIRES,
-    tests_require=["pytest"],
-    license="MIT",
-    keywords="carbon, data, climate",
-    use_scm_version={"version_scheme": "post-release", "local_scheme": "dirty-tag"},
-    setup_requires=["setuptools_scm", "setuptools>=30.3.0"],
+    license='MIT',
+    keywords='carbon, data, climate',
+    use_scm_version={'version_scheme': 'post-release', 'local_scheme': 'dirty-tag'},
 )
