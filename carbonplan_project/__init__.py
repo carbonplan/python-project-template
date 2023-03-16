947b2ec307ccf98dc9344b7cb860c808a80e60f6
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-03-16 17:04:13 -0400

    Update template structure

--- carbonplan_project/__init__.py
+++ carbonplan_project/__init__.py
@@ -1,6 +1,15 @@
 import pathlib
+from importlib.metadata import PackageNotFoundError as _PackageNotFoundError
+from importlib.metadata import version as _version
 
 from intake import open_catalog
+
+try:
+    __version__ = _version(__name__)
+except _PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
+
 
 cat_dir = pathlib.Path(__file__)
 cat_file = str(cat_dir.parent / "data/catalog.yaml")
